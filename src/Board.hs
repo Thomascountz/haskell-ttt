7cb2a773de4f39f9fcbbccb716fe86b2e80d694a
Thomas Countz <thomascountz@gmail.com>
2018-10-10 10:53:35 -0400

    test replacing an Empty cell with Player cell

--- src/Board.hs
+++ src/Board.hs
@@ -5,3 +5,9 @@ type Board = [Cell]
   
 initBoard :: Board
 initBoard = take 9 (repeat Empty)
+
+replaceCellAt :: Board -> Int -> Cell -> Board
+replaceCellAt [] _ _ = []
+replaceCellAt (x:xs) index cell 
+  | index == 0 = cell:xs
+  | otherwise = x:replaceCellAt xs (index - 1) cell
--- test/BoardSpec.hs
+++ test/BoardSpec.hs
@@ -8,5 +8,22 @@ spec =  do
   describe "initBoard" $ do
     it "returns a board with a length of 9" $
       length initBoard `shouldBe` 9
+
     it "returns a board full of Empty" $
-      (foldr (&&) True (map (\cell -> cell == Empty) initBoard)) `shouldBe` True
+      and (map (\cell -> cell == Empty) initBoard) `shouldBe` True
+
+  describe "replaceCellAt" $ do
+    it "returns an empty board if an empty board is given" $
+      replaceCellAt [] 0 Player1 `shouldBe` []
+
+    it "returns board if index is out of bounds" $
+      replaceCellAt [Empty] 2 Player1 `shouldBe` [Empty]
+
+    it "returns a board with a Player1 Cell at index 0" $
+      replaceCellAt [Empty] 0 Player1 !! 0 `shouldBe` Player1
+
+    it "returns a board with a Player2 Cell at index 1" $
+      replaceCellAt [Empty, Empty] 1 Player2 !! 1 `shouldBe` Player2
+
+    it "returns a board with a Player Cell at index 8" $
+      replaceCellAt [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty] 8 Player1 !! 8 `shouldBe` Player1
