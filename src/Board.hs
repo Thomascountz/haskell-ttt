bbbdf86c8c7ebe8d348e18050c521f41b157edaf
Thomas Countz <thomascountz@gmail.com>
2018-10-12 12:11:01 -0400

    code cleanup

--- src/Board.hs
+++ src/Board.hs
@@ -4,9 +4,10 @@ data Cell = Player1 | Player2 | Empty deriving (Eq, Show)
 type Board = [Cell]
   
 initBoard :: Int -> Board
-initBoard size = take (size ^ 2) (repeat Empty)
+initBoard size = replicate (size ^ 2) Empty
 
 size :: Board -> Int
+-- size = round . sqrt . fromIntegral . length
 size board = round (sqrt (fromIntegral (length board)))
 
 replaceCellAt :: Board -> Int -> Cell -> Board
@@ -19,30 +20,31 @@ validPlacement :: Board -> Int -> Bool
 validPlacement board index = index < length board && board !! index == Empty
 
 tie :: Board -> Bool
-tie board = not (win board) && not (any (\x -> x == Empty) board)
+tie board = not (win board || Empty `elem` board)
 
 win:: Board -> Bool
-win board = or (map (winningSpaces board) (winningIndices board))
+win board = any (winningSpaces board) (winningIndices board)
 
 winningSpaces :: Board -> [Int] -> Bool
-winningSpaces board indicies = allTheSameAndNotEmpty (map (\i -> (board !! i)) (indicies))
+winningSpaces board indicies = allTheSameAndNotEmpty (map (\i -> board !! i) indicies)
 
 winningIndices :: Board -> [[Int]]
 winningIndices board = winningRows board ++ winningCols board ++ winningDiags board
 
 allTheSameAndNotEmpty :: [Cell] -> Bool
-allTheSameAndNotEmpty cells = not (head cells == Empty) && and (map (\x -> x == head cells) (tail cells))
+allTheSameAndNotEmpty cells = (head cells /= Empty) && all (\x -> x == head cells) (tail cells)
 
 winningRows :: Board -> [[Int]]
 winningRows board = map (\start -> take (size board) [start..]) (rowStarts board)
 
 winningCols :: Board -> [[Int]]
-winningCols board = map (\start -> take (size board) [start, start + (size board)..]) (colStarts board)
+winningCols board = map (\start -> take (size board) [start, start + size board..]) (colStarts board)
 
 winningDiags :: Board -> [[Int]]
-winningDiags board = take (size board) [0, (size board + 1)..]
-                     : take (size board) [size board - 1, ((size board - 1) ^ 2)..]
-                     : []
+winningDiags board = [
+                      take (size board) [0, (size board + 1)..],
+                      take (size board) [size board - 1, ((size board - 1) ^ 2)..]
+                     ]
 
 rowStarts :: Board -> [Int]
 rowStarts board = take (size board) [0, (0 + size board)..]
@@ -63,10 +65,10 @@ cellStr Empty = "-"
 
 boardStr :: Board -> String
 boardStr board = 
-  ("┌───┬───┬───┐\n" )
+  "┌───┬───┬───┐\n" 
   ++ "| " ++ (cellStr (board !! 0) ++ " | " ++ cellStr (board !! 1) ++ " | " ++ cellStr (board !! 2)) ++ " |\n"
   ++ "│───│───│───│\n"
   ++ "| " ++ (cellStr (board !! 3) ++ " | " ++ cellStr (board !! 4) ++ " | " ++ cellStr (board !! 5)) ++ " |\n"
   ++ "│───│───│───│\n"
   ++ "| " ++ (cellStr (board !! 6) ++ " | " ++ cellStr (board !! 7) ++ " | " ++ cellStr (board !! 8)) ++ " |\n"
-  ++ ("└───┴───┴───┘")
+  ++ "└───┴───┴───┘"
